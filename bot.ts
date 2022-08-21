6fa48ca43bc9836f9a2cd65522674ef14ce93846
kasoqian <95743792+kasoqian@users.noreply.github.com>
2022-08-21 14:18:20 +0800

    feat: 增加注释, 修改provider

--- bot.ts
+++ bot.ts
@@ -1,54 +1,73 @@
-var path = require('path')
-var filename = path.dirname('config.json') + '\\config.json'
+var path = require("path");
 
-const ethers = require('ethers');
-const json = require(filename)
+const ethers = require("ethers");
+const json = require("./config.json");
 
 const addresses = {
   WBNB: json.data.WBNB,
   factory: json.data.factory,
   router: json.data.router,
-  recipient: json.data.recipient
-}
+  recipient: json.data.recipient,
+};
 
-const mnemonic = ''
+/* 助记词配置 */
+const mnemonic = "";
 
-const provider = new ethers.providers.WebSocketProvider('wss://apis.ankr.com/wss/')
-const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+/* 构建provider */
+const provider = new ethers.providers.WebSocketProvider(
+  "https://bsc-dataseed.binance.org",
+);
+
+/* 构建钱包，链接账户 */
+const randomWallet = ethers.Wallet.createRandom(); // 随机钱包
+const _mnemonic = randomWallet._mnemonic().phrase; // 随机钱包的助记词
+
+const wallet = ethers.Wallet.fromMnemonic(_mnemonic);
+
 const account = wallet.connect(provider);
+
+/* 拿到工厂合约 */
 const factory = new ethers.Contract(
   addresses.factory,
-  ['event PairCreated(address indexed token0, address indexed token1, address pair, uint)'],
-  account
-)
+  [
+    "event PairCreated(address indexed token0, address indexed token1, address pair, uint)",
+  ],
+  account,
+);
+
+/* 拿到路由合约 */
 const router = new ethers.Contract(
   addresses.router,
   [
-    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
-    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
+    "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)",
+    "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
   ],
-  account
-)
+  account,
+);
 
+/* 拿到BNB合约-后续交易用 */
 const wbnb = new ethers.Contract(
   addresses.WBNB,
-  [
-    'function approve(address spender) public returns(bool)',
-  ],
-  account
-)
+  ["function approve(address spender) public returns(bool)"],
+  account,
+);
 
+/* 初始化，先进行token授权操作 */
 const init = async () => {
-  const tx = await wbnb.approve(
-    router.address,
-    ethers.constants.MaxUint256
-  )
-  const receipt = await tx.wait()
-  console.log('Transaction receipt')
-  console.log(receipt)
-}
+  const tx = await wbnb.approve(router.address, ethers.constants.MaxUint256);
+  const receipt = await tx.wait();
+  console.log("Transaction receipt");
+  console.log(receipt);
+};
 
-factory.on('PairCreated', async (token0, token1, pairAddress) => {
+/*
+ * 监听工厂合约中的 PairCreated 事件
+ *
+ * token0 通常是流通币
+ * token1 通常是新发币
+ * pairAddress 通常是LP的地址
+ */
+factory.on("PairCreated", async (token0, token1, pairAddress) => {
   console.log(`
     New pair detected
     =================
@@ -58,29 +77,30 @@ factory.on('PairCreated', async (token0, token1, pairAddress) => {
   `);
 
   let tokenIn, tokenOut;
-  if(token0 === addresses.WBNB) {
-    tokenIn = token0
-    tokenOut = token1
+  if (token0 === addresses.WBNB) {
+    tokenIn = token0;
+    tokenOut = token1;
   }
 
-  if(token1 == addresses.WBNB) {
-    tokenIn = token1 
-    tokenOut = token0
+  if (token1 == addresses.WBNB) {
+    tokenIn = token1;
+    tokenOut = token0;
   }
 
-  if(typeof tokenIn === 'undefined') {
-    return
+  if (typeof tokenIn === "undefined") {
+    return;
   }
 
-  const amountIn = ethers.utils.parseUnits('0.001', 'ether');
-  const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut])
-  const amountOutMin = amounts[1].sub(amounts[1].div(10))
+  /* 交易部分 */
+  /*   const amountIn = ethers.utils.parseUnits("0.001", "ether");
+  const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
+  const amountOutMin = amounts[1].sub(amounts[1].div(10));
   console.log(`
     Buying new token
     =================
     tokenIn: ${amountIn.toString()} ${tokenIn} (WBNB)
     tokenOut: ${amounOutMin.toString()} ${tokenOut}
-  `)
+  `);
   const tx = await router.swapExactTokensForTokens(
     amountIn,
     amountOutMin,
@@ -88,13 +108,15 @@ factory.on('PairCreated', async (token0, token1, pairAddress) => {
     addresses.recipient,
     Date.now() + 1000 * 60 * 10, // 10 minutes
     {
-        gasPrice: 5000000000,
-        gasLimit: 1000000
-    }
-  )
-  const receipt = await tx.wait()
-  console.log('Transaction receipt')
-  console.log(receipt)
-})
+      gasPrice: 5000000000,
+      gasLimit: 1000000,
+    },
+  );
+  const receipt = await tx.wait();
+  console.log("Transaction receipt");
+  console.log(receipt); */
+});
 
-init().catch(err => { console.log(err) })
+init().catch(err => {
+  console.log(err);
+});
